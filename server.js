3025e44a50f8868032bdb0f5ed01f542e4759de2
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-11-23 00:54:36 +0100

    update blessings

--- server.js
+++ server.js
@@ -1,72 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
-// const userRoute = require('./routes/userRoute');
-// const paymentRoutes = require('./routes/paymentRoutes');
-// const investmentRoutes = require('./routes/investmentRoutes');
-// const withDrawRoutes = require('./routes/withdrawRoutes');
-// const errorHandler = require('./middleware/errorMiddleware');
-// const seedPlans = require('./utils/seedInvestmentPlans');
-
-// const app = express();
-
-// // Middlewares
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(bodyParser.json());
-
-// app.use(
-//   cors({
-//     // origin: 'https://www.greenwoodsy.com',
-//     origin: ["http://localhost:3000", "https://www.greenwoodsy.com"],
-//     credentials: true,
-//   })
-// );
-
-// // Routes
-// app.use("/api/users", userRoute);
-// app.use("/api/payments", paymentRoutes);
-// app.use("/api/invest", investmentRoutes);
-// app.use("/api/withDraw", withDrawRoutes);
-
-
-
-// app.get("/", (req, res) => {
-//   res.send("Home Page");
-// });
-
-// // Error Handler
-// app.use(errorHandler);
-
-// const PORT = process.env.PORT || 8000;
-
-// // Connect to database and seed plans
-// mongoose.connect(process.env.MONGO_DB_URL).then(async () => {
-//   console.log("Database connected");
-
-//   // Seed investment plans if they don't exist
-//   await seedPlans();
-
-//   app.listen(PORT, () => {
-//     console.log(`Server running on ${PORT}`);
-//   });
-// }).catch((err) => console.error("Database connection error:", err));
-
-
-
-
-
-
-
-
-
-
-
-
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
@@ -89,13 +20,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(bodyParser.json());
 
-app.use(
-  cors({
-    origin: 'https://www.greenwoodsy.com',
-    // origin: ["http://localhost:3000", "https://www.greenwoodsy.com"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'https://www.greenwoodsy.com',
+//     // origin: ["http://localhost:3000", "https://www.greenwoodsy.com"],
+//     credentials: true,
+//   })
+// );
+app.use(cors({ origin: 'https://www.greenwoodsy.com', credentials: true }));
+
 
 // Routes
 app.use("/api/users", userRoute);
